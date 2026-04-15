c88da1173ac4a29a09be4da477990b43f5bd8c6b
E-Krantz <eliaskra@kth.se>
2026-04-15 12:15:07 +0200

    Update atmos airframes

--- src/drivers/uavcan/uavcan_main.cpp
+++ src/drivers/uavcan/uavcan_main.cpp
@@ -1097,7 +1097,7 @@ bool UavcanMixingInterfaceESC::updateOutputs(float outputs[MAX_ACTUATORS], unsig
 		// output_array_size adapts to the highest output index that is mapped (4 for a quad)
 		// this allows for sending less CAN frames depending on what output indices are mapped
 		uint8_t output_array_size = 0;
-		uint16_t disarmed_values[MAX_ACTUATORS]{};
+		uint16_t disarmed_values[MAX_ACTUATORS] {};
 
 		for (int i = MAX_ACTUATORS - 1; i >= 0; i--) {
 			if (mixingOutput().isFunctionSet(i)) {
